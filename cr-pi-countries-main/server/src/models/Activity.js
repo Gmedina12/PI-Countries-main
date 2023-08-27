4e2671e4be082377e9188b51f3bbb50a3c0b5a3e
Gmedina12 <gina.medina.1994@gmail.com>
2023-08-27 01:50:45 -0500

    feat(clients): Add BD and define models

---
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+/** 
+ * MODELO 2 | Activity
+
+ID. *
+Nombre. *
+Dificultad (número del 1 al 5). *
+Duración (en horas).
+Temporada (Verano, Otoño, Invierno o Primavera). *
+*/
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('Country', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dificultad: {
+            type: DataTypes.SMALLINT,
+            allowNull: false,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        },
+        duracion: {
+            type: DataTypes.SMALLINT,
+        },
+        temporada: {
+            type: DataTypes.ENUM('Verano','Primavera', 'Invierno', 'Otoño'),
+            allowNull: false
+        }
+    });
+};
