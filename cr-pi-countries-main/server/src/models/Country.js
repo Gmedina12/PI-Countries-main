2bff9d78d2f5440340610689ab62ee058a9a4d86
Gmedina12 <gina.medina.1994@gmail.com>
2023-08-27 01:50:45 -0500

    feat(server): Add BD and define models

--- cr-pi-countries-main/server/src/models/Country.js
+++ cr-pi-countries-main/server/src/models/Country.js
@@ -1,12 +1,53 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+/**MODELO 1 | Country
+
+ID (Código de tres letras). *
+Nombre. *
+Imagen de la bandera. *
+Continente. *
+Capital. *
+Subregión.
+Área.
+Población. * */
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
-    name: {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey:true,
+      allowNull: false,
+      unique: true
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    bandera: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    continente: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    subregion: {
+      type: DataTypes.STRING
+    },
+    area: {
+      type: DataTypes.STRING
+    },
+    poblacion:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 };
+
+
